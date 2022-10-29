22a7e9584773d15ebec2a89764a7a296315a8203
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2022-10-28 23:36:32 -0300

    etapa 1 finalizada

--- frontend/main.go
+++ frontend/main.go
@@ -1,15 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func listarAlunos() {
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/pedro-barreto/Trabalho-SD/backend/models"
+)
 
-	fmt.Print("Hello World")
+func getAllAlunos() {
+
+	aluno := models.Aluno{}
+
+	db, err := gorm.Open("mysql", "root:@/escola?charset=utf8")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := db.DB().Query("SELECT * FROM alunos")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for res.Next() {
+
+		err := res.Scan(&aluno.ID, &aluno.Nome, &aluno.Idade, &aluno.Telefone)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%v\n", aluno)
+	}
 
 }
 
 func main() {
 
-	listarAlunos()
+	getAllAlunos()
 
 }
